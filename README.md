4919354a7662acfb7c5b3dd1abd747a0f77192de
bengt hedberg <bengt.hedberg@minordkl.com.au>
2023-07-19 16:09:27 +1000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+docker run --rm -it -p 12111-12112:12111-12112 stripe/stripe-mock:latest
---
+++ src/Fraqt.Payment.API/Configuration/StripeOptions.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Fraqt.Payment.Api.Configuration;
+public class StripeOptions
+{
+    public string SecretKey { get; set; }
+}
